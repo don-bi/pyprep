63eb326de2f1a2b2a8da374b07b15988904f4010
don-bi <dbi30@stuy.edu>
2022-09-12 01:05:19 -0400

    added basic index and some styling

--- main.js
+++ main.js
@@ -13,8 +13,42 @@ logos.forEach(logo => {
 })
 
 //header sticky code
-
 window.addEventListener("scroll", function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+//animation for title loading in
+const title = document.querySelector(".main.index .one .title");
+const titleTextArray = title.textContent.split("");
+title.textContent = "";
+
+let tick = 0;
+let char = 0;
+let timer = setInterval(onTick, 50);
+let currentText = title.textContent;
+
+function onTick(){
+    let typing = "";
+    if (tick%12=== 0) typing = "|";
+
+    title.textContent = `${currentText}${typing}`;
+    
+    if ((tick < 34 || tick > 80)) {
+        if (char < titleTextArray.length) {
+            currentText += titleTextArray[char];
+            char ++;
+        }
+        if (tick > 123){
+            complete();
+            return;
+        }
+    }
+    tick ++;
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+    title.textContent = title.substring(0,title.textContent.length-1);
+}
